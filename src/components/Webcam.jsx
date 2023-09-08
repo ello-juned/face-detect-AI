c04642a7391cb52a6d3129e0a5e47b01bd08b267
ellojuned <ello.juned009@gmail.com>
2023-09-08 17:14:30 +0530

    gender & age modal added

--- src/components/Webcam.jsx
+++ src/components/Webcam.jsx
@@ -3,15 +3,17 @@ import * as faceapi from "face-api.js";
 import Loading from "./Loading";
 
 const WebcamComponent = () => {
-  // initiase states---
+  // initialize states---
   const [initializing, setInitializing] = useState(true);
   const [expressions, setExpressions] = useState([]);
+  const [age, setAge] = useState(null);
+  const [gender, setGender] = useState(null);
   const videoRef = useRef();
   const canvasRef = useRef();
   const videoHeight = 400;
   const videoWidth = 400;
 
-  // loading video
+  // loading video and models
   useEffect(() => {
     const loadModels = async () => {
       const MODEL_URL = "/models"; // Update to the correct path
@@ -83,17 +85,23 @@ const WebcamComponent = () => {
           (expression) => expressionsObj[expression] > 0.7
         );
         setExpressions(detectedExpressions);
+
+        // Extract and set age and gender
+        setAge(resizedDetections[0].age);
+        setGender(resizedDetections[0].gender);
       } else {
         setExpressions([]);
+        setAge(null);
+        setGender(null);
       }
     }, 100);
   };
 
   return (
-    <div className="h-screen w-screen  flex flex-col justify-between items-center bg-[#2046F5] p-4">
+    <div className="h-screen w-screen flex flex-col justify-between items-center bg-[#2046F5] p-4">
       {initializing && <Loading />}
 
-      <div className="h-3/12  w-full flex flex-row justify-between  items-center gap-4">
+      <div className="h-3/12 w-full flex flex-row justify-between items-center gap-4">
         <div className="h-auto w-2/12">
           <video
             ref={videoRef}
@@ -106,14 +114,31 @@ const WebcamComponent = () => {
             width={videoWidth}
           />
         </div>
-        <div className="w-full">
-          <h2>
-            Detected Expressions:{" "}
-            {expressions.length > 0 ? expressions.join(", ") : "None"}
-          </h2>
+        <div className="flex flex-row justify-between gap-2 w-full  ">
+          {expressions.length > 0 && (
+            <div className="w-full shadow-xl rounded-lg text-white p-4">
+              <h2 className="mb-2">
+                Detected Expressions:{" "}
+                {expressions.length > 0 ? expressions.join(", ") : "None"}
+              </h2>
+            </div>
+          )}
+
+          {age !== null && (
+            <div className="w-full shadow-xl rounded-lg text-white p-4">
+              <p>Age: {age.toFixed(1)} years</p>
+            </div>
+          )}
+          {gender !== null && (
+            <div className="w-full shadow-xl rounded-lg text-white p-4">
+              <p>Gender: {gender}</p>
+            </div>
+          )}
+
+          {/* Add additional cards here if needed */}
         </div>
       </div>
-      <div className="w-full h-9/12  flex flex-row justify-center items-center bg-[#2046F5]">
+      <div className="w-full h-9/12 flex flex-row justify-center items-center bg-[#2046F5]">
         <canvas
           ref={canvasRef}
           width={videoWidth}
